bc259f934c96718e0f204b2657d775d6a3581a46
Ben C <zxzxlch@gmail.com>
2023-05-30 10:51:12 +0800

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
