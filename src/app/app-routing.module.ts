e3af3aa0998bfda07cae7973bfbef84aeb58febe
Ben C <zxzxlch@gmail.com>
2023-06-01 18:54:48 +0800

    Add page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import {
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { authGuard } from './auth/auth.guard';
 import { LogoutComponent } from './login/logout/logout.component';
+import { Title } from '@angular/platform-browser';
+
+@Injectable({ providedIn: 'root' })
+export class AppTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    const newTitle = title ? `${title} | dbs-uxe-login` : 'dbs-uxe-login';
+    this.title.setTitle(newTitle);
+  }
+}
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'logout', component: LogoutComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    pathMatch: 'full',
+    title: 'DBS Singapore',
+    component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    title: 'Log in to DBS iBanking Singapore',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    canActivate: [authGuard],
+  },
   {
     path: 'dashboard',
+    title: 'DBS iBanking Singapore',
     component: DashboardPageComponent,
     canActivate: [authGuard],
   },
@@ -20,5 +53,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [{ provide: TitleStrategy, useClass: AppTitleStrategy }],
 })
 export class AppRoutingModule {}
