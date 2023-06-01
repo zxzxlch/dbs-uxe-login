b10cb98475ee0086da501995c11e595413b6d88f
Ben C <zxzxlch@gmail.com>
2023-06-01 21:10:53 +0800

    Add documentation for FormFieldComponent

--- src/app/shared/form-field/form-field.component.ts
+++ src/app/shared/form-field/form-field.component.ts
@@ -16,6 +16,23 @@ import {
 import { FormControlName } from '@angular/forms';
 import { FormFieldErrorComponent } from '../form-field-error/form-field-error.component';
 
+/**
+ * FormFieldComponent wraps a label, input (text, email, password, etc.), and an error component.
+ *
+ * Usage:
+ * <app-form-field name="username">
+ *   <label appFormFieldLabel>Username</label>
+ *   <input appFormFieldInput formControlName="username" type="text" required />
+ *   <app-form-field-error></app-form-field-error>
+ * </app-form-field>
+ *
+ * The `appFormFieldLabel` directive should be used for the label element, and the `appFormFieldInput`
+ * directive should be used for the input element. The input field should have the `formControlName` property
+ * assigned as part of your reactive form setup.
+ *
+ * This component automatically adds an id to the input element and the FormFieldError component
+ * based on the provided `name` property. It also adds `htmlFor` and `describedBy` attributes for accessibility.
+ */
 @Component({
   selector: 'app-form-field',
   standalone: true,
